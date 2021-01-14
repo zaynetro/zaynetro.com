06ff0c471fdc5dbcdd0cb1d5b563f979d216042c
Roman Zaynetdinov <roman@zaynetro.com>
2021-01-14 15:43:12 +0200

    Add rockpro64 NixOS config

---
+++ static/rockpro64.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }:
+{
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  # boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
+  # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
+  # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
+  boot.kernelParams = ["cma=32M"];
+
+  # File systems configuration for using the installer's partition layout
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+}
+
+# References:
+# - https://nixos.wiki/wiki/NixOS_on_ARM
+# - https://nixos.wiki/wiki/NixOS_on_ARM/PINE64_ROCKPro64
+# - https://github.com/NixOS/nixpkgs/issues/82455
+# - https://nixos.org/manual/nixos/stable/
