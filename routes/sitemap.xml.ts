79162f44a836cc8d3a842cb7b0f6edbf08d8301d
Roman Zaynetdinov <roman@zaynetro.com>
2023-07-11 22:30:51 +0300

    Add sudoku entries to sitemap

--- routes/sitemap.xml.ts
+++ routes/sitemap.xml.ts
@@ -1,5 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
 import { blogPosts } from "@/build/posts.gen.ts";
+import { exercises } from "@/routes/sudoku/[exercise].tsx";
 
 export const handler: Handlers = {
   GET(req, _ctx) {
@@ -12,12 +13,23 @@ export const handler: Handlers = {
   <loc>${baseUrl}/post/${p.slug}</loc>
 </url>`).join("");
 
+    const sudokuEntries = exercises.map((e) => `
+<url>
+  <loc>${baseUrl}/sudoku/${e.slug}</loc>
+</url>
+`);
+
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>${baseUrl}/</loc>
     </url>
     ${postEntries}
+
+    <url>
+        <loc>${baseUrl}/sudoku</loc>
+    </url>
+    ${sudokuEntries}
 </urlset>
 `;
 
